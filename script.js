8f3849f3e5e0deb0352cc1111ad2722bdf6e7ed4
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2019-10-06 12:44:06 +0100

    Create a function that returns a random hole

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const holes = document.querySelectorAll('.hole');
 const moles = document.querySelectorAll('.mole');
 const scoreboard = document.querySelector('.score');
+let previousHole;
 
-function randTime(min, max) {
+function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+  const idx = Math.round(Math.random() * holes.length);
+  const hole = holes[idx];
+
+  if (hole === previousHole) {
+    return randomHole(holes);
+  }
+  previousHole = hole;
+  return hole;
 }
